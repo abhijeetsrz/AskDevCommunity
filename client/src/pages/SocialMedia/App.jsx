ad1f2c2846c0bf91b36ed7616d8add006c1692b2
Abhijeet <abhi200122002@gmail.com>
2023-12-01 14:18:28 +0530

    saved

--- client/src/pages/SocialMedia/App.jsx
+++ client/src/pages/SocialMedia/App.jsx
@@ -1,8 +1,20 @@
+import { useSelector } from 'react-redux';
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar';
 import './App.css';
 import Home from './pages/home/Home';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+
+  const User = useSelector((state) => (state.currentUserReducer));
+  const Navigate = useNavigate();
+
+  if(!User)
+  {
+     alert("Please Login First");
+     Navigate('/auth/login');
+  }
+
   return (
 
     <div className="App" >
